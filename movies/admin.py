705b39db8f4fca575afe63cb418b6a9884287609
white-ink98 <skorohod.gleb@gmail.com>
2023-06-13 21:48:13 +0300

    add media

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Genre, Movie, MovieShots, Actor, RatingStar, Reviews
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
