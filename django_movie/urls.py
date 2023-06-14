b4f532c3c3102f3b1a18244e4a1851a48ae928d9
white-ink98 <skorohod.gleb@gmail.com>
2023-06-15 02:22:56 +0300

    start urls and static

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("movies.urls")),
 ]
 
 
